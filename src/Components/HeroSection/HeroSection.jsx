afa4e3010546bee441444e3e2fa3fe2b4420a75b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-15 09:03:58 +0330

    Created a data list

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function HeroSection() {
+  const navbars = [
+    {
+      text: "معرفی",
+      href: "#Introduction",
+    },
+    {
+      text: "امکانات",
+      href: "#tools",
+    },
+    {
+      text: "تیم تولید کننده",
+      href: "#team",
+    },
+  ];
   return (
-    <section className='hero-section h-screen w-full flex-col justify-start items-center'>
-        sfsdfsdf
+    <section className="hero-section h-screen w-full flex-col justify-start items-center">
+      <div className="mt-12 w-full max-w-[54.375rem] flex-center py-7">
+        <nav>
+          <ul className="flex-center gap-[6.5rem]"></ul>
+        </nav>
+      </div>
     </section>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
