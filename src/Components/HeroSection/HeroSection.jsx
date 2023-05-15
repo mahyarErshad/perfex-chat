a3eec2cd45f8628ae911b30f333ace51a51e1e4a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-15 10:32:28 +0330

    Created the button

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -18,6 +18,7 @@ function HeroSection() {
   ];
   return (
     <section className="hero-section h-screen w-full flex-col justify-start items-center px-4">
+      {/* Navbars */}
       <div className="header-navbar-container mt-12 w-full max-w-[54.375rem] flex-center py-7">
         <nav>
           <ul className="flex-center gap-[6.5rem]">
@@ -32,6 +33,16 @@ function HeroSection() {
             })}
           </ul>
         </nav>
+      </div>
+      {/* CTA */}
+      <div className="hero-section-cta max-md:mt-8 md:mt-40 w-full max-w-[82.5rem] h-[26.75rem] flex-col items-center">
+        <h1 className="px-6 py-7 font-bold max-md:text-xl md:text-[2.5rem] mt-[4.5rem] text-white h-fit rounded-md">ماژول چت پرفکس</h1>
+        <p className="mt-[3.25rem] text-[#292929] max-md:text-lg md:text-2xl font-semibold">
+          با ماژول <strong className="text-white">” چت پرفکس ”</strong> امکان گفتگو آنلاین بین کارکنان را فراهم کنید.
+        </p>
+        <a href="#aa" target="_blank" className="shadow-md mt-[3.4375rem] bg-white py-3 px-12 text-xl font-semibold text-primary flex-center rounded-3xl">
+          دموی محصول
+        </a>
       </div>
     </section>
   );
