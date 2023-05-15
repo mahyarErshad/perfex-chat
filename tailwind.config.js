ab6414be1024229f2dcf6bb5f91115ab2349cb8e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-15 10:33:37 +0330

    Defined primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        primary: #F77F00,
-      }
+      colors: {
+        primary: "#F77F00",
+      },
     },
   },
   plugins: [],
