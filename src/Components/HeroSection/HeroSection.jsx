68f24f7350fa8c9439fae4cf62a69460770fc2c7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-15 09:22:15 +0330

    Navbars are created

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function HeroSection() {
+  const [selectedNav, setSelectedNav] = useState(0);
   const navbars = [
     {
       text: "معرفی",
@@ -16,10 +17,20 @@ function HeroSection() {
     },
   ];
   return (
-    <section className="hero-section h-screen w-full flex-col justify-start items-center">
-      <div className="mt-12 w-full max-w-[54.375rem] flex-center py-7">
+    <section className="hero-section h-screen w-full flex-col justify-start items-center px-4">
+      <div className="header-navbar-container mt-12 w-full max-w-[54.375rem] flex-center py-7">
         <nav>
-          <ul className="flex-center gap-[6.5rem]"></ul>
+          <ul className="flex-center gap-[6.5rem]">
+            {navbars.map(({ text, href }, index) => {
+              return (
+                <li key={index}>
+                  <a onClick={() => setSelectedNav(index)} className={`inline-block relative text-base duration-300 text-white ${selectedNav === index ? "font-bold navbar-after" : "font-normal"}`} href={href}>
+                    {text}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
         </nav>
       </div>
     </section>
