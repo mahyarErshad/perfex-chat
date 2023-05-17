e9ba45a7d0f8e53a5781726b51cee013a7b75a06
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 10:59:07 +0330

    Side shapes are done

--- src/Components/Utils/SectionTitle/SectionTitle.jsx
+++ src/Components/Utils/SectionTitle/SectionTitle.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function SectionTitle({ title }) {
   return (
     <div className="flex-center gap-6">
-      <span className="w-[0.375rem] bg-primary rounded-[6.25rem]"></span>
-      <h2 className="text-2xl font-bold text-center">{title}</h2>
-      <span className="w-[0.375rem] bg-primary rounded-[6.25rem]"></span>
+      <span className="w-[0.375rem] bg-primary rounded-[6.25rem] h-8"></span>
+      <h2 className="text-2xl font-semibold text-center">{title}</h2>
+      <span className="w-[0.375rem] bg-primary rounded-[6.25rem] h-8"></span>
     </div>
   );
 }
