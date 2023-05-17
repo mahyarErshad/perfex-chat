efd71417125f784705ce9ac79412a33e5d9cc7ff
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 10:55:26 +0330

    Done with this component for now

--- src/Components/Utils/SectionTitle/SectionTitle.jsx
+++ src/Components/Utils/SectionTitle/SectionTitle.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-function SectionTitle() {
+function SectionTitle({ title }) {
   return (
-    <div>SectionTitle</div>
-  )
+    <div className="flex-center gap-6">
+      <span className="w-[0.375rem] bg-primary rounded-[6.25rem]"></span>
+      <h2 className="text-2xl font-bold text-center">{title}</h2>
+      <span className="w-[0.375rem] bg-primary rounded-[6.25rem]"></span>
+    </div>
+  );
 }
 
-export default SectionTitle
+export default SectionTitle;
