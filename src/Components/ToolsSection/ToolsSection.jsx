cf4f2243e43f1a439ef3c17fb8665ea48b7645a5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 11:24:31 +0330

    Added SectionTitle

--- src/Components/ToolsSection/ToolsSection.jsx
+++ src/Components/ToolsSection/ToolsSection.jsx
@@ -4,7 +4,7 @@ import SectionTitle from "../Utils/SectionTitle/SectionTitle";
 function ToolsSection() {
   return (
     <section className="max-md:mt-24 md:mt-[10.625rem] w-full px-4">
-      <SectionTitle />
+      <SectionTitle title="امکانات ماژول چت اسکریپت پرفکس" />
     </section>
   );
 }
