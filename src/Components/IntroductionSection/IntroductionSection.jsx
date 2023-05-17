7f53045a971889d4eac54ebc9f9b317afda154e6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 11:03:18 +0330

    Component is responsive

--- src/Components/IntroductionSection/IntroductionSection.jsx
+++ src/Components/IntroductionSection/IntroductionSection.jsx
@@ -3,7 +3,7 @@ import SectionTitle from "../Utils/SectionTitle/SectionTitle";
 
 function IntroductionSection() {
   return (
-    <section className="mt-[23.5rem] w-full px-4">
+    <section className="mt-48 md:mt-72 lg:mt-[23.5rem] w-full px-4 pb-10">
       <SectionTitle title="ماژول گفتگو آنلاین اسکریپت پرفکس" />
     </section>
   );
--- src/Components/Utils/SectionTitle/SectionTitle.jsx
+++ src/Components/Utils/SectionTitle/SectionTitle.jsx
@@ -4,7 +4,7 @@ function SectionTitle({ title }) {
   return (
     <div className="flex-center gap-6">
       <span className="w-[0.375rem] bg-primary rounded-[6.25rem] h-8"></span>
-      <h2 className="text-2xl font-semibold text-center">{title}</h2>
+      <h2 className="max-md:text-xl md:text-2xl font-semibold text-center">{title}</h2>
       <span className="w-[0.375rem] bg-primary rounded-[6.25rem] h-8"></span>
     </div>
   );
