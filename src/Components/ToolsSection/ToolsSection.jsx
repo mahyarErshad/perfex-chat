dcc4d8babfa96529bec60d8b06f9d751e8f23fc3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 12:51:37 +0330

    Component is responsive

--- src/Components/ToolsSection/ToolsSection.jsx
+++ src/Components/ToolsSection/ToolsSection.jsx
@@ -9,10 +9,10 @@ function ToolsSection() {
   return (
     <section className="max-md:mt-24 md:mt-[10.625rem] w-full px-4">
       <SectionTitle title="امکانات ماژول چت اسکریپت پرفکس" />
-      <Tabs className="flex justify-center gap-6 mt-14">
+      <Tabs className="flex max-lg:flex-col justify-center gap-6 mt-14">
         <div className="flex-col gap-8 lg:max-w-[33.5rem]">
-          <p className="text-base font-medium">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.</p>
-          <TabList className="grid lg:grid-cols-2 grid-tabs-gap">
+          <p className="text-base font-medium max-lg:text-center">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.</p>
+          <TabList className="grid grid-1 max-sm:grid-cols-2 sm:grid-cols-3 lg:grid-cols-2 grid-tabs-gap">
             {tabs.map((tab, index) => {
               return (
                 <Tab key={index}>
