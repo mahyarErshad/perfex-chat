e0fc90c468da985cd2a3d54fe959c662f5212d99
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 11:11:08 +0330

    Min width is 257px

--- src/Components/Utils/MyButton/MyButton.jsx
+++ src/Components/Utils/MyButton/MyButton.jsx
@@ -5,7 +5,7 @@ const MyButton = ({ href, text, white }) => {
   const buttonClass = white ? `${styles["white-button"]}` : `${styles["primary-button"]}`;
   const colorClass = white ? `bg-white text-primary` : `bg-primary text-white`;
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" className={`${styles["my-button"]} ${colorClass} ${buttonClass} shadow-md mt-[3.4375rem] py-3 px-12 text-xl font-semibold flex-center rounded-3xl hover:opacity-90 duration-300 relative`}>
+    <a href={href} target="_blank" rel="noopener noreferrer" className={`${styles["my-button"]} ${colorClass} ${buttonClass} min-w-[13.5625rem] shadow-md mt-[3.4375rem] py-3 px-12 text-xl font-semibold flex-center rounded-3xl hover:opacity-90 duration-300 relative`}>
       {text}
     </a>
   );
