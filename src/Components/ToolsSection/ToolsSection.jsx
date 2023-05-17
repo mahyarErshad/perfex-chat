871dfb9c090b46df63414e97d03b8444b93b09ee
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 11:23:53 +0330

    Imported SectionTitle

--- src/Components/ToolsSection/ToolsSection.jsx
+++ src/Components/ToolsSection/ToolsSection.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import SectionTitle from "../Utils/SectionTitle/SectionTitle";
 
 function ToolsSection() {
   return (
-    <div>ToolsSection</div>
-  )
+    <section className="max-md:mt-24 md:mt-[10.625rem] w-full px-4">
+      <SectionTitle />
+    </section>
+  );
 }
 
-export default ToolsSection
+export default ToolsSection;
