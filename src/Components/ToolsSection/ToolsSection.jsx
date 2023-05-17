64375bb8093da979e97f716ef0017f66fa1a6fb7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 11:27:31 +0330

    Installed react-tabs

--- src/Components/ToolsSection/ToolsSection.jsx
+++ src/Components/ToolsSection/ToolsSection.jsx
@@ -5,6 +5,10 @@ function ToolsSection() {
   return (
     <section className="max-md:mt-24 md:mt-[10.625rem] w-full px-4">
       <SectionTitle title="امکانات ماژول چت اسکریپت پرفکس" />
+      <div className="grid max-md:grid-cols-1 md:grid-cols-2">
+        <div className="flex-col"></div>
+        <div></div>
+      </div>
     </section>
   );
 }
