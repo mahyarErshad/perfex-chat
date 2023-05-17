2252ba5d6570c6d0b42447fdb9d123370b687acc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 10:13:46 +0330

    Using props to make it dynamic

--- src/Components/Utils/MyButton/MyButton.jsx
+++ src/Components/Utils/MyButton/MyButton.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function MyButton() {
+function MyButton({ href, text }) {
   return (
-    <a href="#aa" target="_blank" rel="noopener noreferrer" className="shadow-md mt-[3.4375rem] bg-white py-3 px-12 text-xl font-semibold text-primary flex-center rounded-3xl hover:opacity-90 duration-300 relative">
-      دموی محصول
+    <a href={href} target="_blank" rel="noopener noreferrer" className="my-button shadow-md mt-[3.4375rem] bg-white py-3 px-12 text-xl font-semibold text-primary flex-center rounded-3xl hover:opacity-90 duration-300 relative">
+      {text}
     </a>
   );
 }
