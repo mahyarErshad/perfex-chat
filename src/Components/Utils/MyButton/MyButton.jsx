22afdc45a0d0f24991b3fdc9891134c9eaea6934
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 10:38:05 +0330

    Set shape for white-button class

--- src/Components/Utils/MyButton/MyButton.jsx
+++ src/Components/Utils/MyButton/MyButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./styles.module.css"
 
 function MyButton({ href, text, white }) {
-  const color = white ? "bg-white text-primary" : "bg-primary text-white";
+  const color = white ? `bg-white text-primary ${styles["white-button"]}` : `bg-primary text-white  ${styles["primary-button"]}`;
   return (
     <a href={href} target="_blank" rel="noopener noreferrer" className={`${styles["my-button"]} ${color} shadow-md mt-[3.4375rem] py-3 px-12 text-xl font-semibold flex-center rounded-3xl hover:opacity-90 duration-300 relative`}>
       {text}
