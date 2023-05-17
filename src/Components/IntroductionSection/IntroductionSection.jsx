b0910eb94922504720a06f3388ac3616cf1d03b6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 13:35:02 +0330

    Added aos css

--- src/Components/IntroductionSection/IntroductionSection.jsx
+++ src/Components/IntroductionSection/IntroductionSection.jsx
@@ -4,7 +4,7 @@ import MyButton from "../Utils/MyButton/MyButton";
 
 function IntroductionSection() {
   return (
-    <section id="Introduction" className="mt-48 md:mt-72 lg:mt-[23.5rem] w-full px-4 flex-col items-center md:gap-14 max-md:gap-8">
+    <section data-aos="zoom-out-right" id="Introduction" className="mt-48 md:mt-72 lg:mt-[23.5rem] w-full px-4 flex-col items-center md:gap-14 max-md:gap-8">
       <SectionTitle title="ماژول گفتگو آنلاین اسکریپت پرفکس" />
       <p className="text-base font-medium max-w-[54.5rem] text-center mt-6">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
       <MyButton text="خرید ماژول" href="#" />
