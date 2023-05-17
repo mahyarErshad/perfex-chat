df746a7e4a65d43f36586349abb1abd20d518ef0
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 10:55:58 +0330

    Imported SectionTitle

--- src/Components/IntroductionSection/IntroductionSection.jsx
+++ src/Components/IntroductionSection/IntroductionSection.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import SectionTitle from "../Utils/SectionTitle/SectionTitle";
 
 function IntroductionSection() {
   return (
-    <section className='mt-[23.5rem] w-full px-4'>
-    
+    <section className="mt-[23.5rem] w-full px-4">
+      <SectionTitle title="ماژول گفتگو آنلاین اسکریپت پرفکس" />
     </section>
-  )
+  );
 }
 
-export default IntroductionSection
+export default IntroductionSection;
