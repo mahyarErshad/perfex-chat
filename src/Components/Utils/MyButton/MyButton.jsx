90e0197c1df9424190906dfc41c8da4282bada38
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 10:18:36 +0330

    Defined color

--- src/Components/Utils/MyButton/MyButton.jsx
+++ src/Components/Utils/MyButton/MyButton.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-function MyButton({ href, text }) {
+function MyButton({ href, text, white }) {
+  const color = white ? "bg-white text-primary" : "bg-primary text-white";
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" className="my-button shadow-md mt-[3.4375rem] bg-white py-3 px-12 text-xl font-semibold text-primary flex-center rounded-3xl hover:opacity-90 duration-300 relative">
+    <a href={href} target="_blank" rel="noopener noreferrer" className="my-button shadow-md mt-[3.4375rem] py-3 px-12 text-xl font-semibold flex-center rounded-3xl hover:opacity-90 duration-300 relative">
       {text}
     </a>
   );
