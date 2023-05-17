29609adced5c1cf64b1817221a637b16ee4a6f62
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 10:31:24 +0330

    Using styles module file

--- src/Components/Utils/MyButton/MyButton.jsx
+++ src/Components/Utils/MyButton/MyButton.jsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.css"
 function MyButton({ href, text, white }) {
   const color = white ? "bg-white text-primary" : "bg-primary text-white";
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" className={`my-button ${color} shadow-md mt-[3.4375rem] py-3 px-12 text-xl font-semibold flex-center rounded-3xl hover:opacity-90 duration-300 relative`}>
+    <a href={href} target="_blank" rel="noopener noreferrer" className={`${styles["my-button"]} ${color} shadow-md mt-[3.4375rem] py-3 px-12 text-xl font-semibold flex-center rounded-3xl hover:opacity-90 duration-300 relative`}>
       {text}
     </a>
   );
