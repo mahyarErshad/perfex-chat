8e420736ab23f2808a49b0fd9adff547003312eb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 10:45:58 +0330

    Cleaned and optimized the code

--- src/Components/Utils/MyButton/MyButton.jsx
+++ src/Components/Utils/MyButton/MyButton.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 
-function MyButton({ href, text, white }) {
-  const color = white ? `bg-white text-primary ${styles["white-button"]}` : `bg-primary text-white  ${styles["primary-button"]}`;
+const MyButton = ({ href, text, white }) => {
+  const buttonClass = white ? `${styles["white-button"]}` : `${styles["primary-button"]}`;
+  const colorClass = white ? `bg-white text-primary` : `bg-primary text-white`;
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" className={`${styles["my-button"]} ${color} shadow-md mt-[3.4375rem] py-3 px-12 text-xl font-semibold flex-center rounded-3xl hover:opacity-90 duration-300 relative`}>
+    <a href={href} target="_blank" rel="noopener noreferrer" className={`${styles["my-button"]} ${colorClass} ${buttonClass} shadow-md mt-[3.4375rem] py-3 px-12 text-xl font-semibold flex-center rounded-3xl hover:opacity-90 duration-300 relative`}>
       {text}
     </a>
   );
-}
+};
 
 export default MyButton;
