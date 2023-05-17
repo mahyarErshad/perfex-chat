52d770afe676a9a987b2a0b99e8659bd78339b4d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 12:35:23 +0330

    Font is medium

--- src/Components/ToolsSection/ToolsSection.jsx
+++ src/Components/ToolsSection/ToolsSection.jsx
@@ -10,7 +10,7 @@ function ToolsSection() {
       <SectionTitle title="امکانات ماژول چت اسکریپت پرفکس" />
       <Tabs className="flex justify-center gap-6 mt-14">
         <div className="flex-col gap-8 lg:max-w-[33.5rem]">
-          <p className="text-base font-semibold">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.</p>
+          <p className="text-base font-medium">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.</p>
           <TabList className="grid lg:grid-cols-2 grid-tabs-gap">
             {tabs.map((tab, index) => {
               return (
