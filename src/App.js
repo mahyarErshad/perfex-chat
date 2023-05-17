8be5abd0e185609c72a171bed130d9b6fcf83c23
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-17 11:22:06 +0330

    Imported ToolsSection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./Components/HeroSection/HeroSection";
 import IntroductionSection from "./Components/IntroductionSection/IntroductionSection";
+import ToolsSection from "./Components/ToolsSection/ToolsSection";
 
 function App() {
   return (
     <>
       <HeroSection />
       <IntroductionSection />
+      <ToolsSection />
     </>
   );
 }
