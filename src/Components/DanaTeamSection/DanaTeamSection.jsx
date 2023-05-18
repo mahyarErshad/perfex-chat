ead6fe7f27c91a1b28c26e57c941f8f24e14654b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-18 09:36:10 +0330

    Position of the wrapper is relative

--- src/Components/DanaTeamSection/DanaTeamSection.jsx
+++ src/Components/DanaTeamSection/DanaTeamSection.jsx
@@ -5,7 +5,7 @@ function DanaTeamSection() {
   return (
     <>
       <section id="team" className="w-full px-4 flex-center">
-        <div className="hero-section max-md:mt-24 py-10 md:mt-56 w-full max-w-[82.5rem] rounded-custom flex-center flex-col gap-8">
+        <div className="hero-section relative max-md:mt-24 py-10 md:mt-56 w-full max-w-[82.5rem] rounded-custom flex-center flex-col gap-8">
           <p className="max-md:text-xl md:text-[2rem] text-white font-semibold">ساخته شده با عشـــــق توسط تیم داناتیم</p>
           <MyButton white text="دیگر محصولات" href="#" />
         </div>
