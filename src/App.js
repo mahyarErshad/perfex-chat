480508862c1ee2727af0f22c0bde65223642e04a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-18 08:10:35 +0330

    Imported DanaTeamSection

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import HeroSection from "./Components/HeroSection/HeroSection";
 import IntroductionSection from "./Components/IntroductionSection/IntroductionSection";
 import ToolsSection from "./Components/ToolsSection/ToolsSection";
+import DanaTeamSection from "./Components/DanaTeamSection/DanaTeamSection";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <HeroSection />
       <IntroductionSection />
       <ToolsSection />
+      <DanaTeamSection />
     </>
   );
 }
