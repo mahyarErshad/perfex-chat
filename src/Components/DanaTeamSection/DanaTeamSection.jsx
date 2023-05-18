0d25cdf9e55c92a01bbaf8338d0e8ef80bf93310
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-18 09:38:09 +0330

    Added EmptyHeart to the dom

--- src/Components/DanaTeamSection/DanaTeamSection.jsx
+++ src/Components/DanaTeamSection/DanaTeamSection.jsx
@@ -9,6 +9,7 @@ function DanaTeamSection() {
         <div className="hero-section relative max-md:mt-24 py-10 md:mt-56 w-full max-w-[82.5rem] rounded-custom flex-center flex-col gap-8">
           <p className="max-md:text-xl md:text-[2rem] text-white font-semibold">ساخته شده با عشـــــق توسط تیم داناتیم</p>
           <MyButton white text="دیگر محصولات" href="#" />
+          <EmptyHeart className="absolute top-8 right-28 max-md:hidden" />
         </div>
       </section>
       <div className="w-full h-full max-h-[23.9375rem] footer-background mt-[7rem]"></div>
